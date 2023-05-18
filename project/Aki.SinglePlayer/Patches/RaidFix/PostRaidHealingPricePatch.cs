a7d29409c84f10a4102fd5076790037bce01bb0e
Dev <dev@noreply.dev.sp-tarkov.com>
2023-05-18 15:49:52 +0100

    23122

--- project/Aki.SinglePlayer/Patches/RaidFix/PostRaidHealingPricePatch.cs
+++ project/Aki.SinglePlayer/Patches/RaidFix/PostRaidHealingPricePatch.cs
@@ -2,7 +2,7 @@
 using HarmonyLib;
 using System;
 using System.Reflection;
-using TraderInfo = EFT.Profile.GClass1667;
+using TraderInfo = EFT.Profile.GClass1665;
 
 namespace Aki.SinglePlayer.Patches.RaidFix
 {
