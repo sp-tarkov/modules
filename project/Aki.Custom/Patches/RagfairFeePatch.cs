454b738f45b89d314ebfc199b7b4cdf52d8847b6
Dev <dev@dev.sp-tarkov.com>
2023-09-07 10:03:24 +0100

    Add some saftey around the gclass parameter

--- project/Aki.Custom/Patches/RagfairFeePatch.cs
+++ project/Aki.Custom/Patches/RagfairFeePatch.cs
@@ -10,8 +10,25 @@ namespace Aki.Custom.Patches
 {
     public class RagfairFeePatch : ModulePatch
     {
-        protected override MethodBase GetTargetMethod() => typeof(AddOfferWindow).GetMethod("method_1", PatchConstants.PrivateFlags);
+        public RagfairFeePatch()
+        {
+            // Remember to update prefix parameter if below lines are broken
+            _ = nameof(GClass2842.IsAllSelectedItemSame);
+            _ = nameof(GClass2842.AutoSelectSimilar);
+        }
 
+        protected override MethodBase GetTargetMethod()
+        {
+            return typeof(AddOfferWindow).GetMethod("method_1", PatchConstants.PrivateFlags);
+        }
+
+        /// <summary>
+        /// Calculate tax to charge player and send to server before the offer is sent
+        /// </summary>
+        /// <param name="___item_0">Item sold</param>
+        /// <param name="___gclass2842_0">OfferItemCount</param>
+        /// <param name="___double_0">RequirementsPrice</param>
+        /// <param name="___bool_0">SellInOnePiece</param>
         [PatchPrefix]
         private static void PatchPrefix(ref Item ___item_0, ref GClass2842 ___gclass2842_0, ref double ___double_0, ref bool ___bool_0)
         {
