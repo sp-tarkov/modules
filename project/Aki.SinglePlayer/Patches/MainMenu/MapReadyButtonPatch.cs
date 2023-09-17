b1cef9778bda9a85abd650f809256c54cdd7c9b9
CWX <CWX@noreply.dev.sp-tarkov.com>
2023-09-17 09:48:08 +0100

    ready Button change

---
+++ project/Aki.SinglePlayer/Patches/MainMenu/MapReadyButtonPatch.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using Aki.Reflection.Patching;
+using Aki.Reflection.Utils;
+using BepInEx.Logging;
+using EFT.UI;
+using EFT.UI.Matchmaker;
+
+namespace Aki.SinglePlayer.Patches.MainMenu
+{
+    public class MapReadyButtonPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return typeof(MatchmakerMapPointsScreen).GetMethod("Show", PatchConstants.PrivateFlags);
+        }
+
+        [PatchPostfix]
+        private static void PatchPostFix(ref DefaultUIButton ___readyButton)
+        {
+            ___readyButton?.GameObject?.SetActive(false);
+
+            Logger.Log(LogLevel.Error, "ready button disabled");
+        }
+    }
+}
