54df9e1ec82d897f66f1ff4496ead940d4f2c155
Dev <dev@dev.sp-tarkov.com>
2023-09-17 09:58:45 +0100

    Add comments and cleanup of usings

--- project/Aki.SinglePlayer/Patches/MainMenu/MapReadyButtonPatch.cs
+++ project/Aki.SinglePlayer/Patches/MainMenu/MapReadyButtonPatch.cs
@@ -1,12 +1,15 @@
 ﻿using System.Reflection;
 using Aki.Reflection.Patching;
 using Aki.Reflection.Utils;
-using BepInEx.Logging;
 using EFT.UI;
 using EFT.UI.Matchmaker;
 
 namespace Aki.SinglePlayer.Patches.MainMenu
 {
+    /// <summary>
+    /// Removes the 'ready' button from the map preview screen - accessible by choosing map to deply to > clicking 'map' bottom left of screen
+    /// Clicking the ready button makes a call to client/match/available, something we dont want
+    /// </summary>
     public class MapReadyButtonPatch : ModulePatch
     {
         protected override MethodBase GetTargetMethod()
