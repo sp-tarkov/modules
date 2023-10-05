e8ce90e50771be855ae81a3f0100032f72e96378
CWX <CWX@noreply.dev.sp-tarkov.com>
2023-10-05 17:42:31 +0100

    add try/catch around getting airdropParams as the issue reported was nothing being returned, added check in fixedUpdate before running logic as it does not wait for start/awake for an unknown reason.

--- project/Aki.Custom/Airdrops/AirdropsManager.cs
+++ project/Aki.Custom/Airdrops/AirdropsManager.cs
@@ -1,5 +1,4 @@
-﻿using Aki.Common.Http;
-using Aki.Custom.Airdrops.Models;
+﻿using Aki.Custom.Airdrops.Models;
 using Aki.Custom.Airdrops.Utils;
 using Comfort.Common;
 using EFT;
@@ -16,21 +15,30 @@ namespace Aki.Custom.Airdrops
         public bool isFlareDrop;
         private AirdropParametersModel airdropParameters;
 
-        public async void Start()
+        public async void Awake()
         {
-            var gameWorld = Singleton<GameWorld>.Instance;
-
-            if (gameWorld == null)
+            try
             {
-                Destroy(this);
+                var gameWorld = Singleton<GameWorld>.Instance;
+
+                if (gameWorld == null)
+                {
+                    Destroy(this);
+                }
+
+                airdropParameters = AirdropUtil.InitAirdropParams(gameWorld, isFlareDrop);
+
+                if (!airdropParameters.AirdropAvailable)
+                {
+                    Destroy(this);
+                    return;
+                }
             }
-
-            airdropParameters = AirdropUtil.InitAirdropParams(gameWorld, isFlareDrop);
-
-            if (!airdropParameters.AirdropAvailable)
+            catch
             {
+                Debug.LogError("[AKI-AIRDROPS]: Unable to get config from server, airdrop won't occur");
                 Destroy(this);
-                return;
+                throw;
             }
 
             try
@@ -55,6 +63,8 @@ namespace Aki.Custom.Airdrops
 
         public void FixedUpdate()
         {
+            if (airdropParameters == null || airdropPlane == null || airdropBox == null) return;
+            
             airdropParameters.Timer += 0.02f;
 
             if (airdropParameters.Timer >= airdropParameters.TimeToStart && !airdropParameters.PlaneSpawned)
