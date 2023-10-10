49e4b3fd3a16ed5b872772de0bcff73814603e6b
Dev <dev@dev.sp-tarkov.com>
2023-10-10 12:18:01 +0100

    26535

--- project/Aki.Custom/Patches/ExitWhileLootingPatch.cs
+++ project/Aki.Custom/Patches/ExitWhileLootingPatch.cs
@@ -33,7 +33,7 @@ namespace Aki.Custom.Patches
             var player = Singleton<GameWorld>.Instance.MainPlayer;
             if (profileId == player?.Profile.Id)
             {
-                GClass2897.Instance.CloseAllScreensForced();
+                GClass2898.Instance.CloseAllScreensForced();
             }
 
             return true;
