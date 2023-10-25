a44588b8ac224a3d4d4b0c207731df6331e3b8c1
DrakiaXYZ <565558+TheDgtl@users.noreply.github.com>
2023-10-25 16:29:17 -0700

    Send plugin errors to the server console
    - Implement the models/helpers required to send console messages to the server console
    - Implement sending plugin errors on startup to the server console
    - Remove an unused variable and incorrect comment from PluginErrorNotifierPatch

---
+++ project/Aki.Common/Models/Logging/ServerLogRequest.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.ComponentModel;
+
+namespace Aki.Common.Models.Logging
+{
+    public class ServerLogRequest
+    {
+        public string Source { get; set; }
+        public EServerLogLevel Level { get; set; }
+        public string Message { get; set; }
+        public ServerLogTextColor Color { get; set; }
+        public ServerLogBackgroundColor BackgroundColor { get; set; }
+    }
+}
---
+++ project/Aki.Common/Utils/ServerLog.cs
@@ -0,0 +1,46 @@
+﻿using Aki.Common.Http;
+using Aki.Common.Models.Logging;
+using System;
+
+namespace Aki.Common.Utils
+{
+    public static class ServerLog
+    {
+        public static void Custom(string source, string message, ServerLogTextColor color = null, ServerLogBackgroundColor backgroundColor = null)
+        {
+            Log(source, message, EServerLogLevel.Custom, color, backgroundColor);
+        }
+
+        public static void Error(string source, string message)
+        {
+            Log(source, message, EServerLogLevel.Error);
+        }
+
+        public static void Warn(string source, string message)
+        {
+            Log(source, message, EServerLogLevel.Warn);
+        }
+
+        public static void Success(string source, string message)
+        {
+            Log(source, message, EServerLogLevel.Success);
+        }
+
+        public static void Info(string source, string message)
+        {
+            Log(source, message, EServerLogLevel.Info);
+        }
+
+        public static void Log(string source, string message, EServerLogLevel level = EServerLogLevel.Info, ServerLogTextColor color = null, ServerLogBackgroundColor backgroundColor= null)
+        {
+            ServerLogRequest request = new ServerLogRequest();
+            request.Source = source;
+            request.Message = message;
+            request.Level = level;
+            request.Color = color;
+            request.BackgroundColor = backgroundColor;
+
+            RequestHandler.PostJson("/singleplayer/log", Json.Serialize(request));
+        }
+    }
+}
