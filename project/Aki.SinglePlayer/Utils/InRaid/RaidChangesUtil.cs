18d5c7446a2195b3911d876a19e34f631424fb8c
dwesterwick <dwesterwick@yahoo.com>
2023-12-02 18:06:46 -0500

    Added RaidTimeUtil and updated RaidChangesUtil

--- project/Aki.SinglePlayer/Utils/InRaid/RaidChangesUtil.cs
+++ project/Aki.SinglePlayer/Utils/InRaid/RaidChangesUtil.cs
@@ -30,9 +30,29 @@ namespace Aki.SinglePlayer.Utils.InRaid
         public static string LocationId { get; private set; } = string.Empty;
 
         /// <summary>
+        /// The original escape time for the current (or most recent) raid, in minutes
+        /// </summary>
+        public static int OriginalEscapeTimeMinutes { get; private set; } = int.MaxValue;
+
+        /// <summary>
+        /// The original escape time for the current (or most recent) raid, in seconds
+        /// </summary>
+        public static int OriginalEscapeTimeSeconds => OriginalEscapeTimeMinutes * 60;
+
+        /// <summary>
+        /// The updated escape time for the current (or most recent) raid, in minutes
+        /// </summary>
+        public static int NewEscapeTimeMinutes { get; private set; } = int.MaxValue;
+
+        /// <summary>
+        /// The updated escape time for the current (or most recent) raid, in seconds
+        /// </summary>
+        public static int NewEscapeTimeSeconds => NewEscapeTimeMinutes * 60;
+
+        /// <summary>
         /// The reduction in the escape time for the current (or most recent) raid, in minutes
         /// </summary>
-        public static int RaidTimeReductionMinutes { get; private set; } = 0;
+        public static int RaidTimeReductionMinutes => OriginalEscapeTimeMinutes - NewEscapeTimeMinutes;
 
         /// <summary>
         /// The reduction in the escape time for the current (or most recent) raid, in seconds
@@ -58,7 +78,8 @@ namespace Aki.SinglePlayer.Utils.InRaid
 
             LocationId = raidSettings.SelectedLocation.Id;
 
-            RaidTimeReductionMinutes = raidSettings.SelectedLocation.EscapeTimeLimit - raidChanges.RaidTimeMinutes;
+            OriginalEscapeTimeMinutes = raidSettings.SelectedLocation.EscapeTimeLimit;
+            NewEscapeTimeMinutes = raidChanges.RaidTimeMinutes;
 
             SurvivalTimeReductionSeconds = 0;
             if (raidChanges.NewSurviveTimeSeconds.HasValue)
