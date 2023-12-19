54a6c706f06595d2163caaa4b58ea9d7bb18e5ce
IsWaffle <waffle.lord@noreply.dev.sp-tarkov.com>
2023-12-19 20:23:07 +0000

    update game check (!44)
    Co-authored-by: waffle.lord <waffle.lord@hotmail.com>
    Reviewed-on: https://dev.sp-tarkov.com/SPT-AKI/Modules/pulls/44
    Co-authored-by: IsWaffle <waffle.lord@noreply.dev.sp-tarkov.com>
    Co-committed-by: IsWaffle <waffle.lord@noreply.dev.sp-tarkov.com>

--- project/Aki.Core/Utils/ValidationUtil.cs
+++ project/Aki.Core/Utils/ValidationUtil.cs
@@ -14,20 +14,20 @@ namespace Aki.Core.Utils
 
             try
             {
-                var v1 = Registry.LocalMachine.OpenSubKey(c0, false).GetValue("UninstallString");
+                var v1 = Registry.LocalMachine.OpenSubKey(c0, false).GetValue("InstallLocation");
                 var v2 = (v1 != null) ? v1.ToString() : string.Empty;
-                var v3 = new FileInfo(v2);
+                var v3 = new DirectoryInfo(v2);
                 ServerLog.Debug("Aki.Core", v2);
                 ServerLog.Debug("Aki.Core", string.IsNullOrEmpty(v2) ? "0" : v3.Length.ToString()); 
-
-                var v4 = new FileInfo[]
+                
+                var v4 = new FileSystemInfo[]
                 {
                     v3,
-                    new FileInfo(v2.Replace(v3.Name, @"BattlEye\BEClient_x64.dll")),
-                    new FileInfo(v2.Replace(v3.Name, @"BattlEye\BEService_x64.dll")),
-                    new FileInfo(v2.Replace(v3.Name, @"ConsistencyInfo")),
-                    new FileInfo(v2.Replace(v3.Name, @"Uninstall.exe")),
-                    new FileInfo(v2.Replace(v3.Name, @"UnityCrashHandler64.exe"))
+                    new FileInfo(Path.Join(v2, @"BattlEye\BEClient_x64.dll")),
+                    new FileInfo(Path.Join(v2, @"BattlEye\BEService_x64.dll")),
+                    new FileInfo(Path.Join(v2, "ConsistencyInfo")),
+                    new FileInfo(Path.Join(v2, "Uninstall.exe")),
+                    new FileInfo(Path.Join(v2, "UnityCrashHandler64.exe"))
                 };
 
                 v0 = v4.Length - 1;
