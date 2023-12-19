109e9a08907dd0fa7117690c35d42a881fde0350
Dev <dev@dev.sp-tarkov.com>
2023-12-19 20:44:13 +0000

    Fix build issue

--- project/Aki.Core/Utils/ValidationUtil.cs
+++ project/Aki.Core/Utils/ValidationUtil.cs
@@ -17,18 +17,20 @@ namespace Aki.Core.Utils
                 var v1 = Registry.LocalMachine.OpenSubKey(c0, false).GetValue("InstallLocation");
                 var v2 = (v1 != null) ? v1.ToString() : string.Empty;
                 var v3 = new DirectoryInfo(v2);
-                ServerLog.Debug("Aki.Core", v2);
-                ServerLog.Debug("Aki.Core", string.IsNullOrEmpty(v2) ? "0" : v3.Length.ToString()); 
+                
                 
                 var v4 = new FileSystemInfo[]
                 {
                     v3,
-                    new FileInfo(Path.Join(v2, @"BattlEye\BEClient_x64.dll")),
-                    new FileInfo(Path.Join(v2, @"BattlEye\BEService_x64.dll")),
-                    new FileInfo(Path.Join(v2, "ConsistencyInfo")),
-                    new FileInfo(Path.Join(v2, "Uninstall.exe")),
-                    new FileInfo(Path.Join(v2, "UnityCrashHandler64.exe"))
+                    new FileInfo(Path.Combine(v2, @"BattlEye\BEClient_x64.dll")),
+                    new FileInfo(Path.Combine(v2, @"BattlEye\BEService_x64.dll")),
+                    new FileInfo(Path.Combine(v2, "ConsistencyInfo")),
+                    new FileInfo(Path.Combine(v2, "Uninstall.exe")),
+                    new FileInfo(Path.Combine(v2, "UnityCrashHandler64.exe"))
                 };
+
+                ServerLog.Debug("Aki.Core", new FileInfo(Path.Combine(v2, "UnityCrashHandler64.exe")).Length.ToString());
+                ServerLog.Debug("Aki.Core", new FileInfo(Path.Combine(v2, "Uninstall.exe")).Length.ToString());
 
                 v0 = v4.Length - 1;
 
