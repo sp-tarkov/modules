b2872ad7074a1c9325eecd09efad251d4d447223
CWXDEV <CWXDEV@outlook.com>
2024-01-04 17:16:43 +0000

    Feat: implement achievement popup with data being set to profile for server to save

---
+++ project/Aki.SinglePlayer/Patches/Progression/MidRaidAchievementChangePatch.cs
@@ -0,0 +1,24 @@
+using System.Reflection;
+using Aki.Reflection.Patching;
+using UnityEngine;
+using AchievementsController = GClass3207;
+
+namespace Aki.SinglePlayer.Patches.Progression
+{
+	public class MidRaidAchievementChangePatch : ModulePatch
+	{
+		private static AchievementsController achievementController;
+
+		protected override MethodBase GetTargetMethod()
+		{
+			return typeof(AchievementsController).GetConstructors()[0];
+		}
+
+		[PatchPrefix]
+		private static bool PatchPrefix(ref bool updateAchievements)
+		{
+			updateAchievements = true;
+			return true;
+		}
+	}
+}
