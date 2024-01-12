0cabe9dc1247c4acfa5829aef85a701463920b12
Arys <arys@noreply.dev.sp-tarkov.com>
2024-01-12 08:54:12 +0000

    Shooting the BTR will make it retaliate. Fixed BTR turret from rotating when non-hostile (!56)
    Todo:
    
    * Make the UI notifications for BTR-related stuff appear, e.g. when BTR is about to depart
    * Prevent player from entering BTR when it is hostile to the player
    * Implement cover fire trader service
    * Finish implementation of taxi service
    * Finetune the turret machine gun shooting (it might need some pauses as it will shoot nonstop if it is aiming at you)
    
    Co-authored-by: Nympfonic <arys.steam@gmail.com>
    Reviewed-on: https://dev.sp-tarkov.com/SPT-AKI/Modules/pulls/56
    Co-authored-by: Arys <arys@noreply.dev.sp-tarkov.com>
    Co-committed-by: Arys <arys@noreply.dev.sp-tarkov.com>

---
+++ project/Aki.Debugging/BTR/Patches/BTRTurretCanShootPatch.cs
@@ -0,0 +1,31 @@
+﻿using Aki.Reflection.Patching;
+using EFT.Vehicle;
+using HarmonyLib;
+using System.Reflection;
+using UnityEngine;
+
+namespace Aki.Debugging.BTR.Patches
+{
+    public class BTRTurretCanShootPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(BTRTurretServer), "method_1");
+        }
+
+        [PatchPrefix]
+        public static bool PatchPrefix(BTRTurretServer __instance)
+        {
+            Transform defaultTargetTransform = (Transform)AccessTools.Field(__instance.GetType(), "defaultTargetTransform").GetValue(__instance);
+
+            bool flag = __instance.targetTransform != null && __instance.targetTransform != defaultTargetTransform;
+            bool flag2 = (bool)AccessTools.Method(__instance.GetType(), "method_2").Invoke(__instance, null);
+            bool flag3 = __instance.targetPosition != __instance.defaultAimingPosition;
+
+            var isCanShootProperty = AccessTools.DeclaredProperty(__instance.GetType(), nameof(__instance.IsCanShoot));
+            isCanShootProperty.SetValue(__instance, (flag || flag3) && flag2);
+
+            return false;
+        }
+    }
+}
