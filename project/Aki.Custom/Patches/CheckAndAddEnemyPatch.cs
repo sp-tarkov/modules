a43ea41a75d9f4e8935868bc5fec4574249e2069
Kaeno <kaeno@noreply.dev.sp-tarkov.com>
2024-02-22 16:57:57 +0000

    CheckandaddEnemypatch now returns a bool. Set result to not include isAI Checks. Needs testing (!85)
    Co-authored-by: Kaeno <e>
    Reviewed-on: https://dev.sp-tarkov.com/SPT-AKI/Modules/pulls/85

--- project/Aki.Custom/Patches/CheckAndAddEnemyPatch.cs
+++ project/Aki.Custom/Patches/CheckAndAddEnemyPatch.cs
@@ -15,28 +15,15 @@ namespace Aki.Custom.Patches
         /// <summary>
         /// CheckAndAddEnemy()
         /// Goal: This patch lets bosses shoot back once a PMC has shot them
-        /// removes the !player.AIData.IsAI  check
+        /// Removes the !player.AIData.IsAI  check
+		/// BSG changed the way CheckAndAddEnemy Works in 14.0 Returns a bool now
         /// </summary>
         [PatchPrefix]
-        private static bool PatchPrefix(BotsGroup __instance, IPlayer player, ref bool ignoreAI)
-        {
-            // Z already has player as enemy BUT Enemies dict is empty, adding them again causes 'existing key' errors
-            if (__instance.InitialBotType == WildSpawnType.bossZryachiy || __instance.InitialBotType == WildSpawnType.followerZryachiy)
-            {
-                return false;
-            }
-
-            if (!player.HealthController.IsAlive)
-            {
-                return false; // Skip original
-            }
-
-            if (!__instance.Enemies.ContainsKey(player))
-            {
-                __instance.AddEnemy(player, EBotEnemyCause.checkAddTODO);
-            }            
-
-            return false; // Skip original
+        private static bool PatchPrefix(BotsGroup __instance, IPlayer player, ref bool __result)
+		{
+			// Set result to not include !player.AIData.IsAI checks
+			__result = player.HealthController.IsAlive && !__instance.Enemies.ContainsKey(player) && __instance.AddEnemy(player, EBotEnemyCause.checkAddTODO);
+			return false; // Skip Original
         }
     }
 }
