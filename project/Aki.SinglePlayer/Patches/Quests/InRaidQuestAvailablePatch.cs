eba7d50c67debcf397b7fc664fb36d5c9b6480a7
DrakiaXYZ <565558+TheDgtl@users.noreply.github.com>
2024-03-09 14:25:46 -0800

    Fix LightKeeper tasks not progressing correctly
    To handle in-raid LK task unlocking, when a task is switched from AvailableAfter to Locked, instead switch it to AvailableForStart

---
+++ project/Aki.SinglePlayer/Patches/Quests/InRaidQuestAvailablePatch.cs
@@ -0,0 +1,58 @@
+﻿using Aki.Reflection.Patching;
+using Aki.Reflection.Utils;
+using EFT.Quests;
+using HarmonyLib;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Aki.SinglePlayer.Patches.Quests
+{
+    /**
+     * Lightkeeper quests change their state in-raid, and will change to the `AppearStatus` of the quest once
+     * the AvailableAfter time has been hit. This defaults to `Locked`, but should actually be `AvailableForStart`
+     * 
+     * So if we get a quest state change from `AvailableAfter` to `Locked`, we should actually change to `AvailableForStart`
+     */
+    public class InRaidQuestAvailablePatch : ModulePatch
+    {
+        private static PropertyInfo _questStatusProperty;
+
+        protected override MethodBase GetTargetMethod()
+        {
+            var targetType = PatchConstants.EftTypes.FirstOrDefault(IsTargetType);
+            var targetMethod = AccessTools.Method(targetType, "SetStatus");
+
+            _questStatusProperty = AccessTools.Property(targetType, "QuestStatus");
+
+            Logger.LogDebug($"{this.GetType().Name} Type: {targetType?.Name}");
+            Logger.LogDebug($"{this.GetType().Name} Method: {targetMethod?.Name}");
+            Logger.LogDebug($"{this.GetType().Name} QuestStatus: {_questStatusProperty?.Name}");
+
+            return targetMethod;
+        }
+
+        private bool IsTargetType(Type type)
+        {
+            if (type.GetProperty("StatusTransition") != null &&
+                type.GetProperty("IsChangeAllowed") != null &&
+                type.GetProperty("NeedCountdown") == null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        [PatchPrefix]
+        private static void PatchPrefix(object __instance, ref EQuestStatus status)
+        {
+            var currentStatus = (EQuestStatus)_questStatusProperty.GetValue(__instance);
+
+            if (currentStatus == EQuestStatus.AvailableAfter && status == EQuestStatus.Locked)
+            {
+                status = EQuestStatus.AvailableForStart;
+            }
+        }
+    }
+}
