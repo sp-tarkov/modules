8443e6b5d38f140f94b290d26208ee930460b142
Kaeno <kaeno@noreply.dev.sp-tarkov.com>
2024-03-19 12:54:17 +0000

    Update Description of command

--- project/Aki.Debugging/Patches/ReloadClientPatch.cs
+++ project/Aki.Debugging/Patches/ReloadClientPatch.cs
@@ -22,7 +22,8 @@ namespace Aki.Debugging.Patches
 		}
 
 		[ConsoleCommand("reload", "", null, "Reloads currently loaded profile.\nOnly use while in Main Menu" +
-			"\nRunning command while in hideout will cause graphical glitches and NRE to do with Nightvision. Pretty sure wont cause anything bad")]
+			"\nRunning command while in hideout will cause graphical glitches and NRE to do with Nightvision. Pretty sure wont cause anything bad" +
+			"\nMay Cause Unexpected Behaviors inraid")]
 		public static void Reload()
 		{
 
