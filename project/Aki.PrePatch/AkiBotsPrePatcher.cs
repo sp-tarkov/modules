2147f8dfa3c8ef6afdb39cb52060fa5a6142aac5
DrakiaXYZ <565558+TheDgtl@users.noreply.github.com>
2024-04-16 17:55:22 -0700

    Exit if the user has deleted the `BepInEx/plugins/spt` folder
    Added as a check in the PrePatch, because while users delete `BepInEx/plugins/spt` often, it would be uncommon for them to also delete the prepatcher

--- project/Aki.PrePatch/AkiBotsPrePatcher.cs
+++ project/Aki.PrePatch/AkiBotsPrePatcher.cs
@@ -1,4 +1,8 @@
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using BepInEx.Logging;
 using Mono.Cecil;
 
 namespace Aki.PrePatch
@@ -10,8 +14,21 @@ namespace Aki.PrePatch
         public static int sptUsecValue = 47;
         public static int sptBearValue = 48;
 
+        private static string _sptPluginFolder = "BepInEx/plugins/spt";
+
         public static void Patch(ref AssemblyDefinition assembly)
         {
+            // Make sure the user hasn't deleted the SPT plugins folder
+            if (!ValidateSpt())
+            {
+                string message = $"`{_sptPluginFolder}` or required plugin files not found.\n" +
+                    "Please re-install SPT.\n" +
+                    "Exiting.";
+                MessageBoxHelper.Show(message, "[SPT-AKI] Missing Core Files", MessageBoxHelper.MessageBoxType.OK);
+                Environment.Exit(0);
+                return;
+            }
+
             var botEnums = assembly.MainModule.GetType("EFT.WildSpawnType");
 
             var sptUsec = new FieldDefinition("sptUsec",
@@ -26,6 +43,36 @@ namespace Aki.PrePatch
 
             botEnums.Fields.Add(sptUsec);
             botEnums.Fields.Add(sptBear);
+        }
+
+        private static bool ValidateSpt()
+        {
+            ManualLogSource logger = Logger.CreateLogSource(nameof(AkiBotsPrePatcher));
+
+            // Get the directory our prepatcher is in
+            string assemblyFolder = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+
+            // Validate that the SPT plugin path exists
+            string sptPluginPath = Path.GetFullPath(Path.Combine(assemblyFolder, "..", _sptPluginFolder));
+            if (!Directory.Exists(sptPluginPath))
+            {
+                logger.LogError($"`{_sptPluginFolder}` directory not found");
+                return false;
+            }
+
+            // Validate that the folder exists, and contains our plugins
+            string[] sptPlugins = new string[] { "aki-core.dll", "aki-custom.dll", "aki-singleplayer.dll" };
+            string[] foundPlugins = Directory.GetFiles(sptPluginPath).Select(x => Path.GetFileName(x)).ToArray();
+
+            foreach (string plugin in sptPlugins)
+            {
+                if (!foundPlugins.Contains(plugin))
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
