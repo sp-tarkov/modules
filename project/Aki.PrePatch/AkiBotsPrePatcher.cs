efa7a175c3f74306576eed2bf9c93113322135fa
DrakiaXYZ <drakiaxyz@noreply.dev.sp-tarkov.com>
2024-04-17 07:49:49 +0000

    Exit if the user has deleted the `BepInEx/plugins/spt` folder (!109)
    Added as a check in the PrePatch, because while users delete `BepInEx/plugins/spt` often, it would be uncommon for them to also delete the prepatcher
    
    No idea why there's a second commit in this PR, it doesn't actually change anything. Just Git Things™
    
    Co-authored-by: Terkoiz <terkoiz@spt.dev>
    Co-authored-by: DrakiaXYZ <565558+TheDgtl@users.noreply.github.com>
    Reviewed-on: https://dev.sp-tarkov.com/SPT-AKI/Modules/pulls/109
    Co-authored-by: DrakiaXYZ <drakiaxyz@noreply.dev.sp-tarkov.com>
    Co-committed-by: DrakiaXYZ <drakiaxyz@noreply.dev.sp-tarkov.com>

--- project/Aki.PrePatch/AkiBotsPrePatcher.cs
+++ project/Aki.PrePatch/AkiBotsPrePatcher.cs
@@ -1,4 +1,8 @@
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using BepInEx.Logging;
 using Mono.Cecil;
 
 namespace Aki.PrePatch
@@ -10,8 +14,22 @@ namespace Aki.PrePatch
         public static int sptUsecValue = 49;
         public static int sptBearValue = 50;
 
+        private static string _sptPluginFolder = "plugins/spt";
+
         public static void Patch(ref AssemblyDefinition assembly)
         {
+            // Make sure the user hasn't deleted the SPT plugins folder
+            string assemblyFolder = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            string sptPluginPath = Path.GetFullPath(Path.Combine(assemblyFolder, "..", _sptPluginFolder));
+            if (!ValidateSpt(sptPluginPath))
+            {
+                string message = $"'{sptPluginPath}' or required plugin files not found.\n\n" +
+                    "Please re-install SPT. Exiting.";
+                MessageBoxHelper.Show(message, "[SPT-AKI] Missing Core Files", MessageBoxHelper.MessageBoxType.OK);
+                Environment.Exit(0);
+                return;
+            }
+
             var botEnums = assembly.MainModule.GetType("EFT.WildSpawnType");
 
             var sptUsec = new FieldDefinition("sptUsec",
@@ -26,6 +44,32 @@ namespace Aki.PrePatch
 
             botEnums.Fields.Add(sptUsec);
             botEnums.Fields.Add(sptBear);
+        }
+
+        private static bool ValidateSpt(string sptPluginPath)
+        {
+            ManualLogSource logger = Logger.CreateLogSource(nameof(AkiBotsPrePatcher));
+
+            // Validate that the SPT plugin path exists
+            if (!Directory.Exists(sptPluginPath))
+            {
+                logger.LogError($"'{sptPluginPath}' directory not found");
+                return false;
+            }
+
+            // Validate that the folder exists, and contains our plugins
+            string[] sptPlugins = new string[] { "aki-core.dll", "aki-custom.dll", "aki-singleplayer.dll" };
+            string[] foundPlugins = Directory.GetFiles(sptPluginPath).Select(x => Path.GetFileName(x)).ToArray();
+
+            foreach (string plugin in sptPlugins)
+            {
+                if (!foundPlugins.Contains(plugin))
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
