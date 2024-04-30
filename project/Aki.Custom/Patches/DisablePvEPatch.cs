ebcfca490053ecf52971c93f29f9c17b8b3aaae4
kiobu <kiobu@sdf.org>
2024-04-30 19:42:55 -0400

    fix: disablepve in-raid bug when finding tooltip

--- project/Aki.Custom/Patches/DisablePvEPatch.cs
+++ project/Aki.Custom/Patches/DisablePvEPatch.cs
@@ -21,7 +21,8 @@ namespace Aki.Custom.Patches
         private static bool PatchPrefix(ESessionMode sessionMode, Profile profile, ref GameObject ____notAvailableState)
         {
             ____notAvailableState.SetActive(true);
-            Object.FindObjectsOfType<HoverTooltipArea>().Where(o => o.name == "Locked").Single().SetMessageText("<color=#51c6db>SPT-AKI</color> is already PvE.");
+            Object.FindObjectsOfType<HoverTooltipArea>().Where(o => o.name == "Locked").SingleOrDefault()?.SetMessageText("<color=#51c6db>SPT-AKI</color> is already PvE.");
+
             return false;
         }
     }
