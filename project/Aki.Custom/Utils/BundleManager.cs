64296e3e62e92cc1b4a77f91384e579008872d95
Dev <dev@dev.sp-tarkov.com>
2024-05-12 23:03:30 +0100

    Revert "Improve async bundles (!123)"
    This reverts commit 9e65e68c81f47b05b29244f08d3cb2b9edc8616d.

--- project/Aki.Custom/Utils/BundleManager.cs
+++ project/Aki.Custom/Utils/BundleManager.cs
@@ -10,51 +10,74 @@ namespace Aki.Custom.Utils
 {
     public static class BundleManager
     {
-        private const string CachePath = "user/cache/bundles/";
         private static readonly ManualLogSource _logger;
         public static readonly ConcurrentDictionary<string, BundleItem> Bundles;
+        public static string CachePath;
 
         static BundleManager()
         {
             _logger = Logger.CreateLogSource(nameof(BundleManager));
             Bundles = new ConcurrentDictionary<string, BundleItem>();
+            CachePath = "user/cache/bundles/";
         }
 
         public static string GetBundlePath(BundleItem bundle)
         {
             return RequestHandler.IsLocal
                 ? $"{bundle.ModPath}/bundles/{bundle.FileName}"
-                : CachePath;
+                : CachePath + bundle.FileName;
         }
 
-        public static string GetBundleFilePath(BundleItem bundle)
-        {
-            return GetBundlePath(bundle) + bundle.FileName;
-        }
-
-        public static async Task DownloadManifest()
+        public static async Task GetBundles()
         {
             // get bundles
-            var json = await RequestHandler.GetJsonAsync("/singleplayer/bundles");
+            var json = RequestHandler.GetJson("/singleplayer/bundles");
             var bundles = JsonConvert.DeserializeObject<BundleItem[]>(json);
+
+            // register bundles
+            var toDownload = new ConcurrentBag<BundleItem>();
 
             foreach (var bundle in bundles)
             {
                 Bundles.TryAdd(bundle.FileName, bundle);
+
+                if (await ShouldReaquire(bundle))
+                {
+                    // mark for download
+                    toDownload.Add(bundle);
+                }
+            }
+
+            if (RequestHandler.IsLocal)
+            {
+                // loading from local mods
+                _logger.LogInfo("CACHE: Loading all bundles from mods on disk.");
+                return;
+            }
+            else
+            {
+                // download bundles
+                // NOTE: assumes bundle keys to be unique
+                foreach (var bundle in toDownload)
+                {
+                    // download bundle
+                    var filepath = GetBundlePath(bundle);
+                    var data = await RequestHandler.GetDataAsync($"/files/bundle/{bundle.FileName}");
+                    await VFS.WriteFileAsync(filepath, data);
+                }
             }
         }
 
-        public static async Task DownloadBundle(BundleItem bundle)
+        private static async Task<bool> ShouldReaquire(BundleItem bundle)
         {
-            var filepath = GetBundleFilePath(bundle);
-            var data = await RequestHandler.GetDataAsync($"/files/bundle/{bundle.FileName}");
-            await VFS.WriteFileAsync(filepath, data);
-        }
+            if (RequestHandler.IsLocal)
+            {
+                // only handle remote bundles
+                return false;
+            }
 
-        public static async Task<bool> ShouldReaquire(BundleItem bundle)
-        {
             // read cache
-            var filepath = GetBundleFilePath(bundle);
+            var filepath = CachePath + bundle.FileName;
 
             if (VFS.Exists(filepath))
             {
@@ -65,11 +88,7 @@ namespace Aki.Custom.Utils
                 if (crc == bundle.Crc)
                 {
                     // file is up-to-date
-                    var location = RequestHandler.IsLocal
-                        ? "MOD"
-                        : "CACHE";
-
-                    _logger.LogInfo($"{location}: Loading locally {bundle.FileName}");
+                    _logger.LogInfo($"CACHE: Loading locally {bundle.FileName}");
                     return false;
                 }
                 else
