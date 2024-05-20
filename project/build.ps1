b60b7a6f787048ce48e9be3545e7620f61afc57f
Cj <cj@noreply.dev.sp-tarkov.com>
2024-05-20 10:44:52 +0000

    Normalize build path and naming (!127)
    Normalize the build path for consistency as per https://dev.sp-tarkov.com/SPT-AKI/Issues/issues/658
    
    - All assemblies now live in `BepInEx/plugins/spt`
    - Renamed aki-common.dll and aki-reflection respectively for consistency as well.
    
    Co-authored-by: Cj <161484149+CJ-SPT@users.noreply.github.com>
    Reviewed-on: https://dev.sp-tarkov.com/SPT-AKI/Modules/pulls/127
    Co-authored-by: Cj <cj@noreply.dev.sp-tarkov.com>
    Co-committed-by: Cj <cj@noreply.dev.sp-tarkov.com>

--- project/build.ps1
+++ project/build.ps1
@@ -3,8 +3,6 @@ $bepinexFolder = "..\Build\BepInEx"
 $bepinexPatchFolder = "..\Build\BepInEx\patchers"
 $bepinexPluginFolder = "..\Build\BepInEx\plugins"
 $bepinexSptFolder = "..\Build\BepInEx\plugins\spt"
-$eftDataFolder = "..\Build\EscapeFromTarkov_Data"
-$managedFolder = "..\Build\EscapeFromTarkov_Data\Managed"
 $projReleaseFolder = ".\bin\Release\net471"
 $licenseFile = "..\..\LICENSE.md"
 
@@ -12,14 +10,14 @@ $licenseFile = "..\..\LICENSE.md"
 if (Test-Path "$buildFolder") { Remove-Item -Path "$buildFolder" -Recurse -Force }
 
 # Create build folder and subfolders if they don't exist
-$foldersToCreate = @("$buildFolder", "$bepinexFolder", "$bepinexPatchFolder", "$bepinexPluginFolder", "$bepinexSptFolder", "$eftDataFolder", "$managedFolder")
+$foldersToCreate = @("$buildFolder", "$bepinexFolder", "$bepinexPatchFolder", "$bepinexPluginFolder", "$bepinexSptFolder")
 foreach ($folder in $foldersToCreate) {
     if (-not (Test-Path "$folder")) { New-Item -Path "$folder" -ItemType Directory }
 }
 
 # Move DLLs from project's bin-release folder to the build folder
-Copy-Item "$projReleaseFolder\Aki.Common.dll" -Destination "$managedFolder"
-Copy-Item "$projReleaseFolder\Aki.Reflection.dll" -Destination "$managedFolder"
+Copy-Item "$projReleaseFolder\aki-common.dll" -Destination "$bepinexSptFolder"
+Copy-Item "$projReleaseFolder\aki-reflection.dll" -Destination "$bepinexSptFolder"
 Copy-Item "$projReleaseFolder\aki_PrePatch.dll" -Destination "$bepinexPatchFolder"
 Copy-Item "$projReleaseFolder\aki-core.dll" -Destination "$bepinexSptFolder"
 Copy-Item "$projReleaseFolder\aki-custom.dll" -Destination "$bepinexSptFolder"
