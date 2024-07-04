91607135fcaa292af800f32705d351253b65237d
Nympfonic <arys.steam@gmail.com>
2024-07-04 04:27:29 +0100

    Fixed BTR twerking on steep slopes

---
+++ project/SPT.Custom/BTR/CollisionDebugger.cs
@@ -0,0 +1,35 @@
+﻿using EFT.UI;
+using UnityEngine;
+
+namespace SPT.Custom.BTR
+{
+    public class CollisionDebugger : MonoBehaviour
+    {
+        private int _resetFrame = 10;
+        private int _frame = 0;
+
+        private void Update()
+        {
+            _frame = (_frame + 1) % _resetFrame;
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            foreach (var contact in collision.contacts)
+            {
+                ConsoleScreen.LogWarning($"Collision between {gameObject.name} and {contact.otherCollider.gameObject.name}");
+            }
+        }
+
+        private void OnCollisionStay(Collision collision)
+        {
+            if (_frame == 0)
+            {
+                foreach (var contact in collision.contacts)
+                {
+                    ConsoleScreen.LogWarning($"Collision between {gameObject.name} and {contact.otherCollider.gameObject.name}");
+                }
+            }
+        }
+    }
+}
