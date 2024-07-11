775bad05abcc856e450460d9cfbfe1cd741249ca
CWX <cwxdev@outlook.com>
2024-07-11 21:18:01 +0100

    added comment on patch

--- project/SPT.SinglePlayer/Patches/RaidFix/FixAchievementsPatch.cs
+++ project/SPT.SinglePlayer/Patches/RaidFix/FixAchievementsPatch.cs
@@ -6,6 +6,10 @@ using SPT.Reflection.Utils;
 
 namespace SPT.SinglePlayer.Patches.RaidFix
 {
+    /// <summary>
+    /// Aim of this patch is just to apply the backend session to the controller and supply true to updating achievements,
+    /// the mappings for these also need fixing. 3.8.3 remaps a different class compared to 3.9.0 and 3.10.0.
+    /// </summary>
     public class FixAchievementsPatch : ModulePatch
     {
         protected override MethodBase GetTargetMethod()
