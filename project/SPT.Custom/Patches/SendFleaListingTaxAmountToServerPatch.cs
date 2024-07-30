f1b142ad4937374d24af92a635913c831c37fed6
Dev <dev@dev.sp-tarkov.com>
2024-07-30 10:26:40 +0100

    Improved method comment

--- project/SPT.Custom/Patches/SendFleaListingTaxAmountToServerPatch.cs
+++ project/SPT.Custom/Patches/SendFleaListingTaxAmountToServerPatch.cs
@@ -26,14 +26,14 @@ namespace SPT.Custom.Patches
             return AccessTools.Method(typeof(AddOfferWindow), nameof(AddOfferWindow.method_1));
         }
 
-		/// <summary>
-		/// Calculate tax to charge player and send to server before the offer is sent
-		/// </summary>
-		/// <param name="___item_0">Item sold</param>
-		/// <param name="___gclass3087_0">OfferItemCount</param>
-		/// <param name="___double_0">RequirementsPrice</param>
-		/// <param name="___bool_0">SellInOnePiece</param>
-		[PatchPrefix]
+        /// <summary>
+        /// Calculate tax to charge player and send to server before the offer is sent
+        /// </summary>
+        /// <param name="___item_0">Item sold</param>
+        /// <param name="___ragfairOfferSellHelperClass">OfferItemCount</param>
+        /// <param name="___double_0">RequirementsPrice</param>
+        /// <param name="___bool_0">SellInOnePiece</param>
+        [PatchPrefix]
         private static void PatchPrefix(ref Item ___item_0, ref RagfairOfferSellHelperClass ___ragfairOfferSellHelperClass, ref double ___double_0, ref bool ___bool_0)
         {
             RequestHandler.PutJson("/client/ragfair/offerfees", new
