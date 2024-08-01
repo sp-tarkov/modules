9b86d8624a4864536af1ee573438dfc0f2082690
Kaeno <kaeno@noreply.dev.sp-tarkov.com>
2024-08-01 21:27:35 +0100

    Fix Grenade fuses being longer than they should be

--- project/SPT.Custom/Patches/StunGrenadeExplosionPatch.cs
+++ project/SPT.Custom/Patches/StunGrenadeExplosionPatch.cs
@@ -1,24 +1,25 @@
 ﻿using HarmonyLib;
 using SPT.Reflection.Patching;
+using System;
 using System.Reflection;
 
 namespace SPT.Custom.Patches
 {
 	/// <summary>
-	/// BSG has had the wonderful idea of not letting grenades explode. Delay for the grenades are really long for some reason.
+	/// BSG has had the wonderful idea of not letting grenades explode.
 	/// Waiting on BSG Fix.
 	/// </summary>
 	public class StunGrenadeExplosionPatch : ModulePatch
 	{
 		protected override MethodBase GetTargetMethod()
 		{
-			return AccessTools.Method(typeof(ObservedStunGrenade), nameof(ObservedStunGrenade.method_4));
+			return AccessTools.Method(typeof(ObservedStunGrenade), nameof(ObservedStunGrenade.StartTimer));
 		}
 
 		[PatchPrefix]
-		private static bool PatchPrefix(ObservedStunGrenade __instance)
+		private static bool PatchPrefix(ObservedStunGrenade __instance, float ___float_4)
 		{
-			__instance.InvokeBlowUpEvent();
+			__instance.StartBehaviourTimer(__instance.WeaponSource.GetExplDelay - ___float_4, new Action(__instance.InvokeBlowUpEvent));
 			return false;
 		}
 	}
@@ -27,13 +28,13 @@ namespace SPT.Custom.Patches
 	{
 		protected override MethodBase GetTargetMethod()
 		{
-			return AccessTools.Method(typeof(ObservedGrenade), nameof(ObservedGrenade.method_4));
+			return AccessTools.Method(typeof(ObservedGrenade), nameof(ObservedGrenade.StartTimer));
 		}
 
 		[PatchPrefix]
-		private static bool PatchPrefix(ObservedGrenade __instance)
+		private static bool PatchPrefix(ObservedGrenade __instance, float ___float_4)
 		{
-			__instance.InvokeBlowUpEvent();
+			__instance.StartBehaviourTimer(__instance.WeaponSource.GetExplDelay - ___float_4, new Action(__instance.InvokeBlowUpEvent));
 			return false;
 		}
 	}
