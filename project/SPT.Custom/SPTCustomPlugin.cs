e3bc4e321084675f69ef56fa211797fb527237cd
CWX <cwxdev@outlook.com>
2024-08-02 16:44:14 +0100

    removed more un-needed patches

--- project/SPT.Custom/SPTCustomPlugin.cs
+++ project/SPT.Custom/SPTCustomPlugin.cs
@@ -22,25 +22,20 @@ namespace SPT.Custom
                 new EasyBundlePatch().Enable();
                 
                 // TODO: check if these patches are needed
-                // new BotsGroupLetBossesShootPmcsPatch().Enable();
-                new CustomAiPatch().Enable();
-                new AddTraitorScavsPatch().Enable();
                 new PmcTakesAgesToHealLimbsPatch().Enable();
-                new SendFleaListingTaxAmountToServerPatch().Enable();
 				new DisableNonHalloweenExitsDuringEventPatch().Enable();
-                // new SetLocationIdOnRaidStartPatch().Enable();
                 // new AllScavsHostileHostileToPlayerScavPatch().Enable();
                 // new CopyPmcQuestsToPlayerScavPatch().Enable();
 				// new MergeScavPmcQuestsOnInventoryLoadPatch().Enable();
                 
-                // Needed but needs editing
-                new IsEnemyPatch().Enable();
-                
                 // Still need
+                new SendFleaListingTaxAmountToServerPatch().Enable();
+                new AddTraitorScavsPatch().Enable();
+                new IsEnemyPatch().Enable(); // Might be able to refactor
+                new CustomAiPatch().Enable();
                 new SaveSettingsToSptFolderPatch().Enable();
                 new QTEPatch().Enable();
                 new RedirectClientImageRequestsPatch().Enable();
-                new BotSelfEnemyPatch().Enable();
                 new DisableGameModeAdjustButtonPatch().Enable();
                 new FixPmcSpawnParamsNullErrorPatch().Enable();
                 new SetPreRaidSettingsScreenDefaultsPatch().Enable();
