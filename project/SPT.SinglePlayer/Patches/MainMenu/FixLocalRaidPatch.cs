7bf3a8b840f4b4a62240fb9ce2a4392ad3dba869
Lacyway <20912169+Lacyway@users.noreply.github.com>
2024-09-16 20:16:19 +0200

    Add FixLocalRaidPatch

---
+++ project/SPT.SinglePlayer/Patches/MainMenu/FixLocalRaidPatch.cs
@@ -0,0 +1,26 @@
+﻿using EFT;
+using HarmonyLib;
+using SPT.Reflection.Patching;
+using System.Reflection;
+
+namespace SPT.SinglePlayer.Patches.MainMenu
+{
+	/// <summary>
+	/// This patch ensures that the gamemode is always <see cref="ERaidMode.Local"/> and that IsPveOffline is always true when starting a game<br/>
+	/// This prevents a bug where the gameworld is instantiated as an online world
+	/// </summary>
+	public class FixLocalRaidPatch : ModulePatch
+	{
+		protected override MethodBase GetTargetMethod()
+		{
+			return AccessTools.Method(typeof(TarkovApplication), nameof(TarkovApplication.method_41));
+		}
+
+		[PatchPrefix]
+		public static void Prefix(ref RaidSettings ____raidSettings)
+		{
+			____raidSettings.RaidMode = ERaidMode.Local;
+			____raidSettings.IsPveOffline = true;
+		}
+	}
+}
