bdc25941e8cfe3eb777ac0f3ecc4039700eab2c7
Cj <161484149+CJ-SPT@users.noreply.github.com>
2024-11-20 11:01:35 -0500

    Basic memory manager

---
+++ project/SPT.Custom/Utils/MemoryManager.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Scripting;
+
+namespace SPT.Custom.Utils;
+
+public class MemoryManager : MonoBehaviour
+{
+    private WaitForSecondsRealtime _gcCollectionTime = new(30f);
+
+    public void Awake()
+    {
+        StartCoroutine(MemoryManagerCoroutine());
+    }
+
+    private IEnumerator MemoryManagerCoroutine()
+    {
+        yield return _gcCollectionTime;
+ 
+        // SPTCustomPlugin.Log.LogDebug($"Allocated Mananged Memory {GC.GetTotalMemory(false) / 1024f / 1024f} MB");
+
+        Task.Run(CollectMemory);
+
+        StartCoroutine(MemoryManagerCoroutine());
+    }
+
+    private Task CollectMemory()
+    {
+        GarbageCollector.GCMode = GarbageCollector.Mode.Enabled;
+        GC.Collect(2, GCCollectionMode.Optimized, false, false);
+
+        return Task.CompletedTask;
+    }
+}
