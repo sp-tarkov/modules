25801e3eda79198e62cf42b6484f8fd693ad8569
Cj <cj@noreply.dev.sp-tarkov.com>
2024-11-21 08:54:45 +0000

    GC refactor (!178)
    Make the GC only run on show inventory screen, and only allow it to run for a maximum of 25 milliseconds at a time.
    
    Co-authored-by: Cj <161484149+CJ-SPT@users.noreply.github.com>
    Reviewed-on: https://dev.sp-tarkov.com/SPT/Modules/pulls/178
    Co-authored-by: Cj <cj@noreply.dev.sp-tarkov.com>
    Co-committed-by: Cj <cj@noreply.dev.sp-tarkov.com>

---
+++ project/SPT.Custom/Patches/MemoryCollectionPatch.cs
@@ -0,0 +1,29 @@
+using System.Reflection;
+using Comfort.Common;
+using EFT;
+using EFT.UI;
+using EFT.UI.Screens;
+using HarmonyLib;
+using SPT.Reflection.Patching;
+using UnityEngine.Scripting;
+
+namespace SPT.Custom.Patches;
+
+public class MemoryCollectionPatch : ModulePatch
+{
+    protected override MethodBase GetTargetMethod()
+    {
+        return AccessTools.Method(typeof(MenuTaskBar), nameof(MenuTaskBar.OnScreenChanged));
+    }
+
+    [PatchPostfix]
+    public static void PatchPostfix(EEftScreenType eftScreenType)
+    {
+        if (eftScreenType != EEftScreenType.Inventory || !Singleton<GameWorld>.Instantiated) return;
+
+        GarbageCollector.GCMode = GarbageCollector.Mode.Enabled;
+
+        // 25000000 Nanoseconds is 25 Milliseconds.
+        GarbageCollector.CollectIncremental(25000000L);
+    }
+}
