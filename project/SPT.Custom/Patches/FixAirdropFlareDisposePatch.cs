fa9e6ce35f3a8a2b8b8bc273fff2c996a8c1d67a
Jesse <jesse@archangel.wtf>
2024-11-25 15:47:31 +0100

    Add further fixes to the airdop (#2)
    * Fix airspace congested spam by disposing of the Server airdrop manager as well
    
    * Add patch to dispose of flares correctly
    
    * Spacing fixes

---
+++ project/SPT.Custom/Patches/FixAirdropFlareDisposePatch.cs
@@ -0,0 +1,33 @@
+﻿using SPT.Reflection.Patching;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+namespace SPT.Custom.Patches
+{
+	/// <summary>
+	/// This patch prevents the weird pink smoke / flares that are still in the sky the next raid if a player has just extracted
+	/// while the airplane is dropping a crate
+	/// </summary>
+	public class FixAirdropFlareDisposePatch : ModulePatch
+	{
+		protected override MethodBase GetTargetMethod()
+		{
+			return typeof(GClass2408).GetMethod(nameof(GClass2408.Dispose));
+		}
+
+		[PatchPrefix]
+		public static void Prefix(Dictionary<GameObject, float> ___dictionary_0)
+		{
+			if (___dictionary_0 == null)
+			{
+				return;
+			}
+
+			foreach (KeyValuePair<GameObject, float> keyValuePair in ___dictionary_0)
+			{
+				Object.Destroy(keyValuePair.Key);
+			}
+		}
+	}
+}
