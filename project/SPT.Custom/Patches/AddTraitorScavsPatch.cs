95de6c0175bc628979cf50ad83a69e765c17a158
CWX <cwxdev@outlook.com>
2024-12-26 21:24:31 +0000

    fix tratorpatch

--- project/SPT.Custom/Patches/AddTraitorScavsPatch.cs
+++ project/SPT.Custom/Patches/AddTraitorScavsPatch.cs
@@ -26,7 +26,7 @@ namespace SPT.Custom.Patches
         }
 
         [PatchPrefix]
-        public static bool PatchPrefix(ref BotsGroup __result, IBotGame ____game, DeadBodiesController ____deadBodiesController, BotOwner bot, BotZone zone)
+        public static bool PatchPrefix(ref BotsGroup __result, DeadBodiesController ____deadBodiesController, BotOwner bot, BotZone zone, BotSpawner __instance)
         {
             if (!_traitorChancePercent.HasValue)
             {
@@ -43,7 +43,7 @@ namespace SPT.Custom.Patches
                 var player = Singleton<GameWorld>.Instance.MainPlayer;
                 var enemies = new List<BotOwner>();
                 var players = new List<Player>() { player };
-                var botsGroup = new BotsGroup(zone, ____game, bot, enemies, ____deadBodiesController, players, false);
+                var botsGroup = new BotsGroup(zone, __instance.BotGame, bot, enemies, ____deadBodiesController, players, false);
 
                 // Because we don't want to use the zone-specific group, we add the new group with no key. This is similar to free for all
                 Singleton<IBotGame>.Instance.BotsController.BotSpawner.Groups.AddNoKey(botsGroup, zone);
