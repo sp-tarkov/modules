0f5d029d9b9bc128eb399384ed0ca32063d4e281
CWX <cwxdev@outlook.com>
2024-12-26 21:47:43 +0000

    Update Model for Serverconfig

--- project/SPT.Common/Http/ServerConfig.cs
+++ project/SPT.Common/Http/ServerConfig.cs
@@ -3,12 +3,12 @@
     public class ServerConfig
     {
         public string BackendUrl { get; }
-        public string Version { get; }
+        public string MatchingVersion { get; }
 
-        public ServerConfig(string backendUrl, string version)
+        public ServerConfig(string backendUrl, string matchingVersion)
         {
             BackendUrl = backendUrl;
-            Version = version;
+            MatchingVersion = matchingVersion;
         }
     }
 }
