7bbc92f2fb91b349192f046b1a4ea2950059bf74
Lacyway <20912169+Lacyway@users.noreply.github.com>
2025-01-09 11:15:22 +0100

    Add EnablePrestigeTabPatch

---
+++ project/SPT.Custom/Patches/EnablePrestigeTabPatch.cs
@@ -0,0 +1,40 @@
+﻿using EFT.UI;
+using HarmonyLib;
+using SPT.Reflection.Patching;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace SPT.Custom.Patches
+{
+    /// <summary>
+    /// This transpiler removes the call to <see cref="IBackEndSession.SessionMode"/> to determine if it's <see cref="ESessionMode.Regular"/> <br/>
+    /// and forces a true instead
+    /// </summary>
+    internal class EnablePrestigeTabPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(InventoryScreen.Class2754), nameof(InventoryScreen.Class2754.MoveNext));
+        }
+
+        [PatchTranspiler]
+        public static IEnumerable<CodeInstruction> Transpile(IEnumerable<CodeInstruction> instructions)
+        {
+            CodeInstruction[] codeInstructions = instructions.ToArray();
+
+            // Remove all instructions that gets the gamemode from iSession
+            codeInstructions[259] = new(OpCodes.Nop);
+            codeInstructions[260] = new(OpCodes.Nop);
+            codeInstructions[261] = new(OpCodes.Nop);
+            codeInstructions[262] = new(OpCodes.Nop);
+            codeInstructions[263] = new(OpCodes.Nop);
+
+            // Force a true on the stack instead
+            codeInstructions[264] = new(OpCodes.Ldc_I4_1);
+
+            return codeInstructions;
+        }
+    }
+}
