249123bb48f08391e72428db5712ce603a756319
Chomp <dev@dev.sp-tarkov.com>
2025-01-24 19:09:52 +0000

    Updated to `34644`

--- README.md
+++ README.md
@@ -24,7 +24,7 @@ git config --local user.email "USERNAME@SOMETHING.com"
 
 
 ## Requirements
-- Escape From Tarkov 34568
+- Escape From Tarkov 34644
 - Visual Studio Code -OR- Visual Studio 2022
 - .NET 6 SDK
 - [PowerShell v7](https://learn.microsoft.com/en-us/powershell/scripting/install/installing-powershell-on-windows)
--- project/SPT.Custom/Patches/EnablePrestigeTabPatch.cs
+++ project/SPT.Custom/Patches/EnablePrestigeTabPatch.cs
@@ -13,11 +13,11 @@ namespace SPT.Custom.Patches
     {
         protected override MethodBase GetTargetMethod()
         {
-            return AccessTools.Method(typeof(InventoryScreen.Class2754), nameof(InventoryScreen.Class2754.MoveNext));
+            return AccessTools.Method(typeof(InventoryScreen.Class2751), nameof(InventoryScreen.Class2751.MoveNext));
         }
 
         [PatchPostfix]
-        public static void Postfix(InventoryScreen.Class2754 __instance)
+        public static void Postfix(InventoryScreen.Class2751 __instance)
         {
             var inventoryScreen = __instance.inventoryScreen_0;
             var tabDictionary = Traverse.Create(inventoryScreen).Field<IReadOnlyDictionary<EInventoryTab, Tab>>("_tabDictionary").Value;
