536fda0c17a9397c6c9e91df51aa29aff6ab9594
Lacyway <20912169+Lacyway@users.noreply.github.com>
2025-02-16 20:17:52 +0100

    Modernize MenuNotificationManager

--- project/SPT.Custom/Utils/MenuNotificationManager.cs
+++ project/SPT.Custom/Utils/MenuNotificationManager.cs
@@ -1,13 +1,13 @@
-﻿using SPT.Common.Http;
-using SPT.Common.Utils;
-using SPT.Custom.Models;
-using BepInEx.Bootstrap;
+﻿using BepInEx.Bootstrap;
 using BepInEx.Logging;
 using Comfort.Common;
 using EFT.UI;
+using SPT.Common.Http;
+using SPT.Common.Utils;
+using SPT.Custom.Models;
+using SPT.Custom.Patches;
 using System.Collections.Generic;
 using System.Linq;
-using SPT.Custom.Patches;
 using UnityEngine;
 
 namespace SPT.Custom.Utils
@@ -33,14 +33,55 @@ namespace SPT.Custom.Utils
                 "RuntimeUnityEditor",
                 "com.dirtbikercj.debugplus"
         };
-        
+
         public static string[] disallowedPlugins;
         internal static ReleaseResponse release;
         private bool _isBetaDisclaimerOpen;
         private ManualLogSource _logger;
 
-        // This GClass can be found by looking at ErrorScreen.cs and seeing what the ErrorClass class inherits from: `Window<GClass####>`
+        /// <summary>
+        /// This GClass can be found by looking at <see cref="ErrorScreen"/> and seeing what the ErrorScreen class inherits from: <see cref="Window{T}"/> <br/>
+        /// The constrained generic is the class to use
+        /// </summary>
         private GClass3542 _betaMessageContext;
+
+        /// <summary>
+        /// Retrieves the current build from the registry to check against the current build <br/>
+        /// If this is the first run and no entry exists returns an empty string
+        /// </summary>
+        private string VersionPref
+        {
+            get
+            {
+                return PlayerPrefs.GetString("SPT_Version", string.Empty);
+            }
+        }
+
+        /// <summary>
+        /// Should we show the message, only show if first run or if build has changed
+        /// </summary>
+        private bool ShouldShowBetaMessage
+        {
+            get
+            {
+                return PlayerPrefs.GetInt("SPT_AcceptedBETerms") == 0
+                    && release.isBeta
+                    && !_isBetaDisclaimerOpen;
+            }
+        }
+
+        /// <summary>
+        /// Should we show the release notes, only show on first run or if build has changed
+        /// </summary>
+        private bool ShouldShowReleaseNotes
+        {
+            get
+            {
+                return PlayerPrefs.GetInt("SPT_ShownReleaseNotes") == 0
+                    && !_isBetaDisclaimerOpen
+                    && release.releaseSummaryText != string.Empty;
+            }
+        }
 
         public void Start()
         {
@@ -72,7 +113,7 @@ namespace SPT.Custom.Utils
             {
                 new PreventClientModsPatch().Enable();
             }
-        
+
             if (release.isBeta && PlayerPrefs.GetInt("SPT_AcceptedBETerms") == 1)
             {
                 _logger.LogInfo(release.betaDisclaimerAcceptText);
@@ -107,13 +148,16 @@ namespace SPT.Custom.Utils
             ShowReleaseNotes();
 
             _seenBetaMessage = true;
+
+            // This mono has served its purpose
+            Destroy(this);
         }
 
         // Show the beta message
         // if mods are enabled show that mods are loaded in the message.
         private void ShowBetaMessage()
         {
-            if (Singleton<PreloaderUI>.Instantiated && ShouldShowBetaMessage())
+            if (Singleton<PreloaderUI>.Instantiated && ShouldShowBetaMessage)
             {
                 _betaMessageContext = Singleton<PreloaderUI>.Instance.ShowCriticalErrorScreen(sptVersion, release.betaDisclaimerText, ErrorScreen.EButtonType.OkButton, release.betaDisclaimerTimeoutDelay);
                 // Note: This looks backwards, but a timeout counts as "Accept" and clicking the button counts as "Decline"
@@ -127,7 +171,7 @@ namespace SPT.Custom.Utils
         // Show the release notes.
         private void ShowReleaseNotes()
         {
-            if (Singleton<PreloaderUI>.Instantiated && ShouldShowReleaseNotes())
+            if (Singleton<PreloaderUI>.Instantiated && ShouldShowReleaseNotes)
             {
                 Singleton<PreloaderUI>.Instance.ShowCriticalErrorScreen(sptVersion, release.releaseSummaryText, ErrorScreen.EButtonType.OkButton, 36000);
                 PlayerPrefs.SetInt("SPT_ShownReleaseNotes", 1);
@@ -161,7 +205,7 @@ namespace SPT.Custom.Utils
         // Return true if changed, false if not
         private void SetVersionPref()
         {
-            if (GetVersionPref() == string.Empty || GetVersionPref() != sptVersion)
+            if (VersionPref == string.Empty || VersionPref != sptVersion)
             {
                 PlayerPrefs.SetString("SPT_Version", sptVersion);
 
@@ -169,25 +213,6 @@ namespace SPT.Custom.Utils
                 PlayerPrefs.SetInt("SPT_AcceptedBETerms", 0);
                 PlayerPrefs.SetInt("SPT_ShownReleaseNotes", 0);
             }
-        }
-
-        // Retrieves the current build from the registry to check against the current build
-        // If this is the first run and no entry exists returns an empty string
-        private string GetVersionPref()
-        {
-            return PlayerPrefs.GetString("SPT_Version", string.Empty);
-        }
-
-        // Should we show the message, only show if first run or if build has changed
-        private bool ShouldShowBetaMessage()
-        {
-            return PlayerPrefs.GetInt("SPT_AcceptedBETerms") == 0 && release.isBeta && !_isBetaDisclaimerOpen;
-        }
-
-        // Should we show the release notes, only show on first run or if build has changed
-        private bool ShouldShowReleaseNotes()
-        {
-            return PlayerPrefs.GetInt("SPT_ShownReleaseNotes") == 0 && !_isBetaDisclaimerOpen && release.releaseSummaryText != string.Empty;
-        }
+        }        
     }
 }
