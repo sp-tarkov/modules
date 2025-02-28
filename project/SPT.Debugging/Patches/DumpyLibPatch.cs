bad9dd88f0a79de5f54ecbfce707d2c1dd412903
CWX <cwxdev@outlook.com>
2025-02-28 21:10:36 +0000

    updated changes needed for dumper for client 35328

--- project/SPT.Debugging/Patches/DumpyLibPatch.cs
+++ project/SPT.Debugging/Patches/DumpyLibPatch.cs
@@ -1,4 +1,8 @@
-﻿using System.Reflection;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using BepInEx.Logging;
 using EFT;
 using EFT.Bots;
 using EFT.UI;
@@ -8,6 +12,7 @@ using JsonType;
 using SPT.Reflection.Patching;
 using SPT.Reflection.Utils;
 using UnityEngine;
+using Logger = BepInEx.Logging.Logger;
 
 namespace SPT.Debugging.Patches;
 
@@ -38,18 +43,28 @@ public class DumplyLibMono : MonoBehaviour
     public LocalRaidSettings _localRaidSettings;
     public RaidSettings _raidSettings;
     public LocationSettingsClass _locationSettings;
+    public List<LocationSettingsClass.Location> _locationSettingsDict;
     public GClass1959 _endRaidClass;
     public GClass1998 _completeProfile;
     public GClass825 _parsedDataProfile;
-    // Class references are as of assembly 35312 - 28/02/2025
+    public ManualLogSource _dumpLogger;
+    // Class references are as of assembly 35328 - 28/02/2025
 
     private void Start()
     {
+        _dumpLogger = Logger.CreateLogSource(nameof(DumplyLibMono));
+        Task.Factory.StartNew(() => StartTask(), TaskCreationOptions.LongRunning);
+    }
+
+    private async Task StartTask()
+    {
+        await Task.Delay(5000);
+
         _session = ClientAppUtils.GetClientApp().Session as Class303;
         _tarkovApplication = ClientAppUtils.GetMainApp();
-        _mainMenuController = _tarkovApplication.GetType().GetField("mainMenuController"); // is null at this point so only get fieldinfo - TODO: fieldinfo came back as null
-        _wavesSettings = new WaveInfoClass(2, WildSpawnType.assault, BotDifficulty.normal); // imitate loading json of wave settings
-        _localRaidSettings = new LocalRaidSettings // this has changed from current repo json TODO: check dumped data for changes here
+        _mainMenuController = _tarkovApplication.GetType().GetField("mainMenuController");
+        _wavesSettings = new WaveInfoClass(2, WildSpawnType.assault, BotDifficulty.normal);
+        _localRaidSettings = new LocalRaidSettings
         {
             serverId = null,
             location = "Interchange",
@@ -57,7 +72,7 @@ public class DumplyLibMono : MonoBehaviour
             mode = ELocalMode.TRAINING,
             playerSide = ESideType.Pmc,
         };
-        _raidSettings = new RaidSettings // this has changed from current repo json TODO: check dumped data for changes here
+        _raidSettings = new RaidSettings
         {
             KeyId = null,
             LocationId = "Interchange",
@@ -88,9 +103,15 @@ public class DumplyLibMono : MonoBehaviour
             },
             Side = ESideType.Pmc,
             RaidMode = ERaidMode.Online,
-            PlayersSpawnPlace = EPlayersSpawnPlace.SamePlace
+            PlayersSpawnPlace = EPlayersSpawnPlace.SamePlace,
+            OnlinePveRaid = false
         };
+        _locationSettingsDict = _session.LocationSettings.locations.Values.ToList();
         _locationSettings = _session.LocationSettings;
+        _raidSettings.GetType()
+            .GetField("_locationSettings", BindingFlags.NonPublic | BindingFlags.Instance)
+            .SetValue(_raidSettings, _locationSettings);
+
         _endRaidClass = new GClass1959
         {
             profile = null,
@@ -108,5 +129,10 @@ public class DumplyLibMono : MonoBehaviour
 
         _parsedDataProfile = _completeProfile.ToUnparsedData();
         _endRaidClass.profile = _completeProfile.ToUnparsedData();
+    }
+
+    private void TestMethod()
+    {
+        _raidSettings.UpdateOnlinePveRaidStates();
     }
 }
