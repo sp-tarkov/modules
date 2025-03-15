d8da8f9706e881ea1b72bf1a39d2f50b47331484
Chomp <dev@dev.sp-tarkov.com>
2025-03-15 17:10:54 +0000

    Removed unnecessary patch

--- project/SPT.SinglePlayer/SPTSingleplayerPlugin.cs
+++ project/SPT.SinglePlayer/SPTSingleplayerPlugin.cs
@@ -71,9 +71,6 @@ namespace SPT.SinglePlayer
 				new PmcBotSidePatch().Enable();
 				new QuestAchievementRewardInRaidPatch().Enable();
 
-                // 3.10.x specific, should be removed for 4.0.x
-                new QuestLoadStatusChangePatch().Enable();
-
             }
 			catch (Exception ex)
 			{
