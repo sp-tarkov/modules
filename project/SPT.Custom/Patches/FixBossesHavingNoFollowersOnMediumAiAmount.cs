e3c2dcb05d1599a4a76380a74dbf114f78a55718
Chomp <dev@dev.sp-tarkov.com>
2025-03-27 10:15:42 +0000

    Fixed PMCs not spawning

--- project/SPT.Custom/Patches/FixBossesHavingNoFollowersOnMediumAiAmount.cs
+++ project/SPT.Custom/Patches/FixBossesHavingNoFollowersOnMediumAiAmount.cs
@@ -1,4 +1,5 @@
-﻿using SPT.Reflection.Patching;
+﻿using System;
+using SPT.Reflection.Patching;
 using System.Reflection;
 using EFT;
 using EFT.Bots;
@@ -8,6 +9,7 @@ namespace SPT.Custom.Patches
 {
     /// <summary>
     /// Fix bosses not spawning with any followers because BSG subtract the escort amount from itself
+    /// Also fix 'enable bosses' checkbox not working
     /// </summary>
     public class FixBossesHavingNoFollowersOnMediumAiAmount: ModulePatch
     {
@@ -17,20 +19,28 @@ namespace SPT.Custom.Patches
         }
 
         [PatchPrefix]
-        public static void PatchPrefix(LocalGame __instance, WavesSettings wavesSettings, ref BossLocationSpawn[] bossLocationSpawn, ref bool isPVEOffline)
+        public static void PatchPrefix(LocalGame __instance, WavesSettings wavesSettings, ref BossLocationSpawn[] bossLocationSpawn)
         {
-            // Flag as false to allow player to disable bosses in pre-raid screen
-            isPVEOffline = false;
+            // Player has disabled bosses, filter to only PMCs
+            if (!wavesSettings.IsBosses)
+            {
+                bossLocationSpawn =
+                    Array.FindAll(bossLocationSpawn, x => x.BossType is WildSpawnType.pmcBEAR or WildSpawnType.pmcUSEC);
 
-            // Not a boss and not set to medium, skip
-            if (wavesSettings is not { IsBosses: true, BotAmount: EBotAmount.Medium })
+                return;
+            }
+
+            // Not set to medium = nothing to fix, skip
+            if (wavesSettings.BotAmount != EBotAmount.Medium)
             {
                 return;
             }
 
+            // Adjust boss escort amount values
             foreach (var locationSpawn in bossLocationSpawn)
             {
                 // Only adjust bosses with single escort amount value, skip others
+                // e.g. skip "1,3,5,2"
                 if (locationSpawn.BossEscortAmount.Length != 1)
                 {
                     continue;
@@ -40,12 +50,11 @@ namespace SPT.Custom.Patches
                 var existingAmount = int.Parse(locationSpawn.BossEscortAmount);
                 if (existingAmount == 0)
                 {
-                    // Don't add more data to boss with 0 followers
                     continue;
                 }
 
                 // Add second value to property, when client does (max - min / 2), it won't be 0
-                // e.g. reshala has value of "4", we add value "(existing)*3 (=12)", which means the client does "(12 - 4) / 2" giving us 4 escorts - same as high/as online ai amount
+                // e.g. Reshala has value of "4", we add value "(existing)*3 (=12)", which means the client does "(12 - 4) / 2" giving us 4 escorts - same as high/as online AI amount
                 locationSpawn.BossEscortAmount += $",{existingAmount * 3}";
             }
         }
