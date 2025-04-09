995b1ae256b0b00c6e90da0011e8cf3043473eb6
CWX <cwxdev@outlook.com>
2025-04-09 18:33:09 +0100

    update ref patch

--- project/SPT.SinglePlayer/Patches/MainMenu/EnableRefIntermScreenPatch.cs
+++ project/SPT.SinglePlayer/Patches/MainMenu/EnableRefIntermScreenPatch.cs
@@ -12,7 +12,7 @@ namespace SPT.SinglePlayer.Patches.MainMenu
     {
         protected override MethodBase GetTargetMethod()
         {
-            return AccessTools.Method(typeof(MerchantsList.Class2926), nameof(MerchantsList.Class2926.method_0));
+            return AccessTools.Method(typeof(MerchantsList.Class2925), nameof(MerchantsList.Class2925.method_0));
         }
 
         [PatchPrefix]
