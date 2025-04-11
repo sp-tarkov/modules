5d6bcbf2162824c6ecf9a8b7d8e2c05d298f0ff6
KaenoDev <193943350+KaenoDev@users.noreply.github.com>
2025-04-11 16:22:04 +0100

    Fix patch targeting wrong method

--- project/SPT.SinglePlayer/Patches/ScavMode/EnablePlayerScavPatch.cs
+++ project/SPT.SinglePlayer/Patches/ScavMode/EnablePlayerScavPatch.cs
@@ -13,7 +13,7 @@ namespace SPT.SinglePlayer.Patches.ScavMode
 		/// </summary>
 		protected override MethodBase GetTargetMethod()
         {
-            return AccessTools.Method(typeof(MainMenuControllerClass), nameof(MainMenuControllerClass.method_25));
+            return AccessTools.Method(typeof(MainMenuControllerClass), nameof(MainMenuControllerClass.method_26));
         }
 
 		[PatchPrefix]
