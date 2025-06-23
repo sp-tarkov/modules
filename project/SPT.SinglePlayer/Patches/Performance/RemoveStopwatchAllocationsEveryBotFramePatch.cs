044b0bbae786dc75f513f39aab5539a5bd616906
Lacyway <20912169+Lacyway@users.noreply.github.com>
2025-06-23 22:39:29 +0200

    Make RemoveStopwatchAllocationsEveryBotFramePatch dynamic

--- project/SPT.SinglePlayer/Patches/Performance/RemoveStopwatchAllocationsEveryBotFramePatch.cs
+++ project/SPT.SinglePlayer/Patches/Performance/RemoveStopwatchAllocationsEveryBotFramePatch.cs
@@ -1,20 +1,30 @@
 ﻿using EFT;
 using HarmonyLib;
 using SPT.Reflection.Patching;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
 
 namespace SPT.SinglePlayer.Patches.Performance
 {
     /// <summary>
-    /// Transpiler used to stop the allocation of a new <see cref="Stopwatch"/> every frame for all active AI <br/>
-    /// To update transpiler, look for: <br/>
-    /// - New allocation of <see cref="Stopwatch"/> <br/>
-    /// - <see cref="Stopwatch.Start"/> and <see cref="Stopwatch.Stop"/> <br/>
-    /// - Unnecessary run of <see cref="BotUnityEditorRunChecker.ManualLateUpdate"/>
+    /// Transpiler used to stop the allocation of a new <see cref="Stopwatch"/> every frame for all active AI.
+    /// <para>
+    /// To update the transpiler, check for the following:
+    /// </para>
+    /// <list type="bullet">
+    ///   <item>
+    ///     <description>New allocation of <see cref="Stopwatch"/></description>
+    ///   </item>
+    ///   <item>
+    ///     <description>Calls to <see cref="Stopwatch.Start"/> and <see cref="Stopwatch.Stop"/></description>
+    ///   </item>
+    ///   <item>
+    ///     <description>Unnecessary execution of <see cref="BotUnityEditorRunChecker.ManualLateUpdate"/></description>
+    ///   </item>
+    /// </list>
     /// </summary>
     public class RemoveStopwatchAllocationsEveryBotFramePatch : ModulePatch
     {
@@ -24,22 +34,46 @@ namespace SPT.SinglePlayer.Patches.Performance
         }
 
         [PatchTranspiler]
-        public static IEnumerable<CodeInstruction> Transpile(IEnumerable<CodeInstruction> instructions)
+        public static IEnumerable<CodeInstruction> Transpile(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
         {
-            List<CodeInstruction> codeList = instructions.ToList();
+            Type stopWatchType = typeof(Stopwatch);
 
-            // These 3 lines remove BotUnityEditorRunChecker.ManualLateUpdate()
-            codeList[115] = new CodeInstruction(OpCodes.Nop);
-            codeList[114] = new CodeInstruction(OpCodes.Nop);
-            codeList[113].opcode = OpCodes.Nop;
+            CodeMatcher matcher = new(instructions, generator);
+            matcher.Start();
 
-            // These 4 remove the allocation of the Stopwatch and the Start() and Stop()
-            codeList[18] = new CodeInstruction(OpCodes.Nop);
-            codeList[14] = new CodeInstruction(OpCodes.Nop);
-            codeList[13] = new CodeInstruction(OpCodes.Nop);
-            codeList[12] = new CodeInstruction(OpCodes.Nop);
+            CodeMatch[] inst1 = [
+                new(OpCodes.Newobj, stopWatchType.GetConstructor([])),
+                new(OpCodes.Dup),
+                new(OpCodes.Callvirt, stopWatchType.GetMethod(nameof(Stopwatch.Start)))
+            ];
 
-            return codeList;
+            matcher.MatchForward(false, inst1)
+                .ThrowIfInvalid("Could not find Stopwatch allocation in IL instructions");
+
+            matcher.InstructionAt(inst1.Length).labels.AddRange(matcher.Instruction.labels);
+            matcher.Instruction.labels.Clear();
+            matcher.RemoveInstructions(inst1.Length);
+
+            matcher.MatchForward(false,
+                new CodeMatch(OpCodes.Callvirt, stopWatchType.GetMethod(nameof(Stopwatch.Stop))))
+                .ThrowIfInvalid("Could not find Stopwatch.Stop method in IL instructions");
+
+            matcher.RemoveInstruction();
+
+            CodeMatch[] inst2 = [
+                new(OpCodes.Ldarg_0),
+                new(OpCodes.Call, typeof(BotOwner).GetProperty(nameof(BotOwner.UnityEditorRunChecker)).GetGetMethod()),
+                new(OpCodes.Callvirt, typeof(BotUnityEditorRunChecker).GetMethod(nameof(BotUnityEditorRunChecker.ManualLateUpdate)))
+            ];
+
+            matcher.MatchForward(false, inst2)
+                .ThrowIfInvalid("Could not find call to BotUnityEditorRunChecker.ManualLateUpdate in IL instructions");
+
+            matcher.InstructionAt(inst2.Length).labels.AddRange(matcher.Instruction.labels);
+            matcher.Instruction.labels.Clear();
+            matcher.RemoveInstructions(inst2.Length);
+
+            return matcher.Instructions();
         }
     }
 }
