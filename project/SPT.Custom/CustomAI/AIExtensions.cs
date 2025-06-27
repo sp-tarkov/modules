f3a94fd7a29fc1d4f35215bcc72b30b8071cb4ee
Lacyway <20912169+Lacyway@users.noreply.github.com>
2025-06-27 23:20:13 +0200

    Cleanup pt 2

--- project/SPT.Custom/CustomAI/AIExtensions.cs
+++ project/SPT.Custom/CustomAI/AIExtensions.cs
@@ -3,22 +3,33 @@ using System.Collections.Generic;
 
 namespace SPT.Custom.CustomAI
 {
-    public static class AiHelpers
+    public static class AIExtensions
     {
         /// <summary>
-        /// Bot is a PMC when it has IsStreamerModeAvailable flagged and has a wildspawn type of 'pmcBEAR' or 'pmcUSEC'
+        /// Determines whether the bot is a PMC based on its <see cref="EPlayerSide"/>  
         /// </summary>
-        /// <param name="botRoleToCheck">Bots role</param>
-        /// <param name="botOwner">Bot details</param>
-        /// <returns></returns>
+        /// <param name="botOwner">Bot details to evaluate</param>
+        /// <returns>
+        /// <see langword="true"/> if the bot is a PMC <br/>
+        /// <see langword="false"/> if the bot is not a PMC (i.e. a scav)
+        /// </returns>
         public static bool IsPMC(this BotOwner botOwner)
         {
             return botOwner.Profile.Side != EPlayerSide.Savage;
         }
 
+        /// <summary>
+        /// Determines if the bot owner represents a simulated player scav
+        /// </summary>
+        /// <param name="botOwner">The bot owner instance to evaluate</param>
+        /// <returns>
+        /// <see langword="true"/> if the bot owner's role is assault and the main profile nickname is not empty <br/>
+        /// <see langword="false"/> otherwise
+        /// </returns>
         public static bool IsSimulatedPlayerScav(this BotOwner botOwner)
         {
-            return botOwner.Profile.Info.Settings.Role == WildSpawnType.assault && !string.IsNullOrEmpty(botOwner.Profile.Info.MainProfileNickname);
+            return botOwner.Profile.Info.Settings.Role == WildSpawnType.assault
+                && !string.IsNullOrEmpty(botOwner.Profile.Info.MainProfileNickname);
         }
 
         public static List<BotOwner> GetAllMembers(this BotsGroup group)
