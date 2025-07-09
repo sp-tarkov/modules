ce914669a1eb0780a85555e626aab138155535de
Archangel <jesse@archangel.wtf>
2025-07-09 16:59:40 +0200

    Update patch

--- project/SPT.SinglePlayer/Patches/MainMenu/DisableMatchmakerPlayerPreviewButtonsPatch.cs
+++ project/SPT.SinglePlayer/Patches/MainMenu/DisableMatchmakerPlayerPreviewButtonsPatch.cs
@@ -13,7 +13,7 @@ namespace SPT.SinglePlayer.Patches.MainMenu
         }
 
         [PatchPrefix]
-        public static void PatchPrefix(ref GClass3532 contextInteractions)
+        public static void PatchPrefix(ref ContextInteractionsClass contextInteractions)
         {
             // clear with a null to stop "looking for group/create group" buttons
             // they handle nulls so don't worry
