4ab53d37fb80b0edf0bc6165a51fdfa963cca7ed
Cj <161484149+CJ-SPT@users.noreply.github.com>
2025-09-07 14:07:44 -0400

    Port patching work from server (#63)
    * Implement patch cache and patch manager
    
    * Use Try..Catch, fix counts

--- project/SPT.Reflection/Patching/Attributes.cs
+++ project/SPT.Reflection/Patching/Attributes.cs
@@ -22,3 +22,15 @@ public class PatchFinalizerAttribute : Attribute { }
 [MeansImplicitUse]
 [AttributeUsage(AttributeTargets.Method)]
 public class PatchILManipulatorAttribute : Attribute { }
+
+/// <summary>
+///     If added to a patch, it will not be used during auto patching
+/// </summary>
+[AttributeUsage(AttributeTargets.Class)]
+public class IgnoreAutoPatchAttribute : Attribute;
+
+/// <summary>
+///     If added to a patch, it will only be enabled during debug builds
+/// </summary>
+[AttributeUsage(AttributeTargets.Class)]
+public class DebugPatchAttribute : Attribute;
