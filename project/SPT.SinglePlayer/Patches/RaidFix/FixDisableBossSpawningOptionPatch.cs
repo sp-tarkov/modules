29437f0dc9960070dced1ca5ee41c540a2f22eb6
Archangel <jesse@archangel.wtf>
2025-12-26 12:13:30 +0100

    Update boss spawning patch to exclude PMC's (Fixes: https://github.com/sp-tarkov/server-csharp/issues/714)

--- project/SPT.SinglePlayer/Patches/RaidFix/FixDisableBossSpawningOptionPatch.cs
+++ project/SPT.SinglePlayer/Patches/RaidFix/FixDisableBossSpawningOptionPatch.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Reflection;
-using System.Text;
 using EFT;
 using HarmonyLib;
 using SPT.Reflection.Patching;
@@ -9,7 +7,7 @@ using SPT.Reflection.Patching;
 namespace SPT.SinglePlayer.Patches.RaidFix;
 
 /// <summary>
-/// The purpose of this patch is to remove the isPVEOffline check from the if statement 
+/// The purpose of this patch is to remove bosses from the spawning pool if they are turned off, but not PMCs (as they are seen as bosses)
 /// </summary>
 public class FixDisableBossSpawningOptionPatch : ModulePatch
 {
@@ -18,14 +16,16 @@ public class FixDisableBossSpawningOptionPatch : ModulePatch
         return AccessTools.Method(typeof(LocalGame), nameof(LocalGame.smethod_8));
     }
 
-
-    [PatchTranspiler]
-    public static IEnumerable<CodeInstruction> PatchTranspiler(IEnumerable<CodeInstruction> originalInstructions)
+    [PatchPrefix]
+    public static bool PatchPrefix(WavesSettings wavesSettings, BossLocationSpawn[] bossLocationSpawn, ref BossLocationSpawn[] __result)
     {
-        var instructionsList = new List<CodeInstruction>(originalInstructions);
+        // We only need to filter out the bosses here, not the PMCs
+        if (!wavesSettings.IsBosses)
+        {
+            __result = Array.FindAll(bossLocationSpawn, boss => boss.BossName is "pmcUSEC" or "pmcBEAR");
+        }
 
-        instructionsList.RemoveRange(3, 2);
-
-        return instructionsList;
+        // Skip the original here, the original doesn't run any code anyway due to checks against if we are in PVE and offline
+        return false;
     }
 }
