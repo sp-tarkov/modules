a7df06a662fb3669c39be67df6d2ad28733882de
Cj <161484149+CJ-SPT@users.noreply.github.com>
2026-04-28 04:25:42 -0400

    transfer ownership of `DisableDevMaskCheckPatch` to asm tool

--- project/SPT.SinglePlayer/SPTSingleplayerPlugin.cs
+++ project/SPT.SinglePlayer/SPTSingleplayerPlugin.cs
@@ -55,7 +55,6 @@ public class SPTSingleplayerPlugin : BaseUnityPlugin
 
             // 3.11.0
             new ScavPrestigeFixPatch().Enable();
-            new DisableDevMaskCheckPatch().Enable();
             new DisableUseBSGServersCheckbox().Enable();
             new PmcBotSidePatch().Enable();
             new QuestAchievementRewardInRaidPatch().Enable();
@@ -70,6 +69,7 @@ public class SPTSingleplayerPlugin : BaseUnityPlugin
             // Disabled: Handled by AsmTool
             //new LocaleFixPatch().Enable();
             //new RemoveStopwatchAllocationsEveryCoverPointFramePatch().Enable();
+            //new DisableDevMaskCheckPatch().Enable();
         }
         catch (Exception ex)
         {
