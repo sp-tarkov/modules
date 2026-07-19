6680abc4d9c589cb76d51329d35802a544c8dd48
Archangel <jesse@archangel.wtf>
2026-07-19 22:00:17 +0200

    Use prop again

--- project/SPT.Custom/CustomAI/PmcFoundInRaidEquipment.cs
+++ project/SPT.Custom/CustomAI/PmcFoundInRaidEquipment.cs
@@ -149,7 +149,7 @@ public class PmcFoundInRaidEquipment(ManualLogSource logger)
 
     private void MakeEquipmentNotFiR(BotOwner ___botOwner_0)
     {
-        var additionalItems = ___botOwner_0.Profile.InventoryInfo.GetItemsInSlots([
+        var additionalItems = ___botOwner_0.Profile.Inventory.GetItemsInSlots([
             EquipmentSlot.Backpack,
             EquipmentSlot.FirstPrimaryWeapon,
             EquipmentSlot.SecondPrimaryWeapon,
